439efba76875e650fbd6899e1d14d1b58fbc7bcc
bshzzhsb <sibo_he@qq.com>
2022-01-27 23:16:41 +0800

    feat: add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react-hooks/recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
   rules: {
     semi: ['error', 'always'],
     quotes: ['error', 'single'],

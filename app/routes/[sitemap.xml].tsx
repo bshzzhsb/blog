90632be9b224e01acadcba02f9a22932c0a605e4
bshzzhsb <sibo_he@qq.com>
2023-03-26 20:52:30 +0800

    feat: sitemap.xml and robots.txt

---
+++ app/routes/[sitemap.xml].tsx
@@ -0,0 +1,43 @@
+import { getBlogList } from '~/api/blog.server';
+
+const time = new Date().toISOString();
+
+export const loader = async () => {
+  const blogList = await getBlogList();
+
+  const blogs = blogList
+    .map(
+      blog => `
+<url>
+  <loc>https://blog.bshz.xyz/${blog.slug}</loc>
+  <lastmod>${time}</lastmod>
+  <priority>1.0</priority>
+</url>`,
+    )
+    .join();
+
+  const content = `
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://blog.bshz.xyz/</loc>
+    <lastmod>${time}</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://blog.bshz.xyz/playground</loc>
+    <lastmod>${time}</lastmod>
+    <priority>1.0</priority>
+  </url>
+  ${blogs}
+</urlset>
+`;
+
+  return new Response(content, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml',
+      'xml-version': '1.0',
+      encoding: 'UTF-8',
+    },
+  });
+};
